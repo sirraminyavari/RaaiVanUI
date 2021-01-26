0c33a167212fddf2a7e6ebd917812b86fba92447
Rasoul678 <h.rostami.r@gmail.com>
2021-01-26 12:50:34 +0330

    Add lazy to router and error boundry

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { PrivateRoute, PublicRoute } from "utils/RouteHandler";
 import { Provider } from "react-redux";
+import store from "store";
+import Spinner from "components/Spinner";
+import ErrorBoundry from "components/ErrorBoundry/ErrorBoundry";
+
+//TODO: Move to redux Provider
 import { RVGlobalProvider } from "context/RVGlobalProvider";
 import { ThemeProvider } from "context/ThemeProvider";
-import store from "store";
-import MainLayout from "layouts/Main";
-import Login from "views/Auth/Login";
+
+const MainLayout = lazy(() => import("layouts/Main"));
+const Login = lazy(() => import("views/Auth/Login"));
 
 render(
   <React.StrictMode>
     <Provider store={store}>
       <RVGlobalProvider>
         <ThemeProvider>
-          <Router>
-            <Switch>
-              <PublicRoute exact path="/login" component={Login} />
-              <PrivateRoute path="/" component={MainLayout} />
-            </Switch>
-          </Router>
+          <ErrorBoundry>
+            <Suspense fallback={<Spinner />}>
+              <Router>
+                <Switch>
+                  <PublicRoute exact path="/login" component={Login} />
+                  <PrivateRoute path="/" component={MainLayout} />
+                </Switch>
+              </Router>
+            </Suspense>
+          </ErrorBoundry>
         </ThemeProvider>
       </RVGlobalProvider>
     </Provider>
