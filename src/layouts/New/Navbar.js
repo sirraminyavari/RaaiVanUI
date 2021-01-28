7632d719993b21ce2eeb92449253e96d82cef982
Rasoul678 <h.rostami.r@gmail.com>
2021-01-28 11:13:52 +0330

    Add nav botton

--- src/layouts/New/Navbar.js
+++ src/layouts/New/Navbar.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
-import Avatar from "components/Avatar";
-import SearchIcon from "assets/icons/search.svg";
-import SearchSelectedIcon from "assets/icons/search-selected.svg";
+import { useState } from 'react';
+import Avatar from 'components/Avatar';
+import SearchIcon from 'assets/icons/search.svg';
+import SearchSelectedIcon from 'assets/icons/search-selected.svg';
+import SocialIcon from 'assets/icons/social.svg';
+import TeamsIcon from 'assets/icons/teams.svg';
+import MessagesIcon from 'assets/icons/messages.svg';
+import NotificationsIcon from 'assets/icons/notifications.svg';
+import NavbarButton from './NavbarButton';
 
 const NewNavbar = () => {
   const [isFocused, setIsFocused] = useState(false);
@@ -11,81 +16,58 @@ const NewNavbar = () => {
   return (
     <div
       style={{
-        backgroundColor: "#2B7BE4",
-        height: "11%",
-        overflow: "hidden",
-        position: "fixed",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
+        backgroundColor: '#2B7BE4',
+        height: '11%',
+        overflow: 'hidden',
+        position: 'fixed',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         top: 0,
-        width: "calc(100% - 250px)",
-        padding: "0 20px",
+        width: 'calc(100% - 250px)',
+        padding: '0 20px',
         zIndex: 1,
-        boxShadow: "0px 3px 10px #333"
-      }}
-    >
-      <div style={{ height: "100%", display: "flex" }}>
-        <div style={{ position: "relative", padding: "0 10px" }}>
-          <div>NavButton</div>
-          <div
-            style={{
-              position: "absolute",
-              bottom: -4.9,
-              right: 0,
-              width: "100%",
-              borderTop: "8px solid #fff",
-              borderRadius: 4
-            }}
-          ></div>
-        </div>
-        <div style={{ position: "relative", padding: "0 10px", margin: "0 5px" }}>
-          <div>NavButton</div>
-          <div
-            style={{
-              position: "absolute",
-              bottom: -4.9,
-              right: 0,
-              width: "100%",
-              borderTop: "8px solid #fff",
-              borderRadius: 4
-            }}
-          ></div>
-        </div>
+        boxShadow: '0px 3px 10px #333',
+      }}>
+      <div style={{ height: '100%', display: 'flex' }}>
+        <NavbarButton label="اجتماعی" icon={SocialIcon} />
+        <NavbarButton label="تیم ها" icon={TeamsIcon} />
+        <NavbarButton label="پیام ها" icon={MessagesIcon} />
+        <NavbarButton label="اعلان ها" icon={NotificationsIcon} badge={99} />
       </div>
       <div
         style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-end",
-          width: "400px",
-        }}
-      >
-        <div style={{ position: "relative" }}>
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+          width: '400px',
+        }}>
+        <div style={{ position: 'relative' }}>
           <img
             src={isFocused ? SearchSelectedIcon : SearchIcon}
             style={{
-              position: "absolute",
+              position: 'absolute',
               left: 5,
               top: 5,
             }}
+            alt="search-icon"
           />
           <input
             type="search"
             placeholder={
-              isFocused ? "" : "جستجو در مطالب،کاربران،ابزارها و ..."
+              isFocused ? '' : 'جستجو در مطالب،کاربران،ابزارها و ...'
             }
             onFocus={handleWidth}
             onBlur={handleWidth}
             style={{
-              border: "none",
-              borderRadius: "5px",
+              border: 'none',
+              borderRadius: '5px',
               outline: 0,
-              color: "#333",
-              height: "32px",
-              padding: "0 10px",
+              color: '#333',
+              height: '32px',
+              padding: '0 10px',
               width: isFocused ? 300 : 230,
-              transition: "all 0.5s ease",
+              transition: 'all 0.5s ease',
             }}
           />
         </div>
