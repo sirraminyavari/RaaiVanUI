d8eebe07cbaa642c6f7c183badaf08d0b82409d2
Rasoul678 <h.rostami.r@gmail.com>
2021-02-06 18:33:12 +0330

    Use apiHandler inside usCheckRoute

--- src/hooks/useCheckRoute.js
+++ src/hooks/useCheckRoute.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useQuery from 'hooks/useQuery';
+import APIHandler from 'apiHelper/APIHandler';
 
 const useCheckRoute = (name) => {
   const [result, setResult] = useState({});
   const routeParams = useParams();
   const queryParams = useQuery();
   const params = { ...routeParams, ...queryParams };
+  const apiHandler = new APIHandler('RVAPI', 'CheckRoute');
 
   useEffect(() => {
-    window.RVAPI.CheckRoute({
-      RouteName: name,
-      Parameters: params,
-      ParseResults: true,
-      ResponseHandler: (response) => {
-        setResult(response);
-      },
-    });
+    apiHandler.fetch(
+      { RouteName: name, Parameters: params, ParseResults: true },
+      (response) => setResult(response),
+      (error) => console.log(error)
+    );
 
     return () => {
       setResult({});
