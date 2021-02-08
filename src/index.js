1b7d596f2bfee6f5a61cd17f9338dfb8b5ba15a5
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-02-08 12:26:10 +0330

    Back to route handler for check auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { PrivateRoute, PublicRoute } from 'utils/RouteHandler';
 import StoreProvider from 'store/StoreProvider';
 import Spinner from 'components/Spinner';
 import ErrorBoundry from 'components/ErrorBoundry/ErrorBoundry';
@@ -8,7 +9,8 @@ import ErrorBoundry from 'components/ErrorBoundry/ErrorBoundry';
 //TODO: Move to redux Provider
 import { RVGlobalProvider } from 'context/RVGlobalProvider';
 
-import MainLayout from 'layouts/Main';
+const MainLayout = lazy(() => import('layouts/Main'));
+const Login = lazy(() => import('views/Auth/Login'));
 
 render(
   <React.StrictMode>
@@ -18,7 +20,8 @@ render(
           <Suspense fallback={<Spinner />}>
             <Router>
               <Switch>
-                <Route path="/" component={MainLayout} />
+                <PublicRoute exact path="/login" component={Login} />
+                <PrivateRoute path="/" component={MainLayout} />
               </Switch>
             </Router>
           </Suspense>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,10 @@ import { lazy } from 'react';
 import NavbarRoutes from './Navbar.routes';
 import SidebarRoutes from './Sidebar.routes';
 const ErrorView = lazy(() => import('views/Error'));
-const Login = lazy(() => import('views/Auth/Login'));
 const NewNode = lazy(() => import('views/NewNode'));
 const ChangePassword = lazy(() => import('views/ChangePassword'));
 
 const routes = [
-  {
-    path: '/login',
-    name: 'login',
-    exact: true,
-    hasNavSide: false,
-    component: Login,
-  },
   ...NavbarRoutes,
   ...SidebarRoutes,
   {
