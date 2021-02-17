feb27daff0cd33b09a935c38aa40ddcb30c8d4df
Ali Kargar <aligol20@gmail.com>
2021-02-17 08:49:56 +0330

    Deleted

--- src/store/actions/auth/loginRouteAction.js
+++
@@ -1,8 +0,0 @@
-import { loginRouteSlice } from '../../reducers/loginRouteReducer';
-
-const { changeLoginRoute } = loginRouteSlice.actions;
-
-const setLoginRoute = (params) => (dispatch) => {
-  dispatch(changeLoginRoute(params));
-};
-export default setLoginRoute;
