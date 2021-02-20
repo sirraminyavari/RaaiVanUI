096b34878dd9eb64f62e1e6c446c7f5e2c3feb91
Ali Kargar <aligol20@gmail.com>
2021-02-20 07:51:00 +0330

    A mother component for all login components.

--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -1,89 +1,149 @@
 /**
- * A mother component for handling all login events.
+ * A mother component for all login components.
  */
 import Loader from 'components/Loader/Loader';
 import Logo from 'components/Media/Logo';
-import React, { Suspense, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useMediaQuery } from 'react-responsive';
-import styled from 'styled-components';
+import { RESET_PASSWORD } from 'const/LoginRoutes';
+import { decode } from 'js-base64';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import setLoginRouteAction from 'store/actions/auth/setLoginRouteAction';
+import setOrgDomainsAction from 'store/actions/auth/setOrgDomainsAction';
 import ContinueWithGoogle from './elements/ContinueWithGoogle';
 import CreateAccountButton from './elements/CreateAccountButton';
+import Description from './elements/Description';
 import Email from './elements/Email';
 import ForgotPassword from './elements/ForgotPassword';
-import NavigateButton from './elements/NavigateButton';
+import NameFamily from './elements/NameFamily';
+import NavigationButton from './elements/NavigationButton';
+import OrgDomains from './elements/OrgDomains';
 import Password from './elements/Password';
-import WavyBackground from './items/WavyBackground';
-import Description from './elements/Description';
+import PasswordValidation from './elements/PasswordValidation';
+import ResendCode from './elements/ResendCode';
 import Return from './elements/Return';
+import Title from './elements/Title';
+import VerificationCode from './elements/VerificationCode';
+import {
+  BackgroundImage,
+  Box,
+  Center,
+  Container,
+  Maintainer,
+} from './Login.style';
 
 /**
- * A function that handle custom routing between login's screen such as :
- * login,signup,reset password,...
+ * A mother component for containing the login elements.
  */
 const Login = () => {
-  const { currentRoute } = useSelector((state) => state.loginRoute);
-  const state = useSelector((state) => state);
-  const [fullScreen, setFullScreen] = useState(false);
+  // True, if required files for the login page has been loaded.
+  const [loadDone, setLoadDone] = useState(false);
+  // True, if activationCode of reset password ticket has been checked.
+  const [preinitDone, setPreinitDone] = useState(false);
+  // True, if org domains has been checked.(final step)
+  const [oneStepToInitDone, setOneStepToInit] = useState(false);
 
-  // Use React.lazy to increase page loading speed.
+  const dispatch = useDispatch();
 
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1000px)' });
-  // It checks screen dimension.
+  useEffect(() => {
+    const { GlobalUtilities } = window;
+    let files = [{ Root: 'API/', Ext: 'js', Childs: ['RVAPI', 'UsersAPI'] }];
+    // if (that.Options.UseCaptcha) files.push("CaptchaImage.js");
+    GlobalUtilities.load_files(files, {
+      OnLoad: () => {
+        setLoadDone(true);
+      },
+    });
+    console.log(window, '<--- window');
+  }, []);
+  useEffect(() => {
+    if (loadDone) {
+      const { GlobalUtilities, UsersAPI, RVDic } = window;
+      const reqParams = GlobalUtilities.request_params();
+      const activationCode = reqParams.get_value('ac');
+      const userName = reqParams.get_value('un');
+      const passwordTicket = reqParams.get_value('pt');
+
+      if (activationCode) {
+        UsersAPI.ActivateTemporaryUser({
+          ActivationCode: activationCode,
+          ResponseHandler: function (responseText) {
+            const result = JSON.parse(responseText);
+            if (result.Succeed)
+              alert(RVDic.MSG[result.Succeed], { Timeout: 30000 });
+            if (result.ErrorText) alert(RVDic.MSG[result.ErrorText]);
+            setPreinitDone(true);
+          },
+        });
+      } else if (passwordTicket) {
+        // that.show_reset_password_form(passwordTicket, userName);
+        dispatch(setLoginRouteAction(RESET_PASSWORD));
+        setPreinitDone(true);
+      } else setPreinitDone(true);
+    }
+  }, [loadDone]);
+  useEffect(() => {
+    const { RVAPI, SAASBasedMultiTenancy } = window;
+    if (!SAASBasedMultiTenancy) {
+      RVAPI.GetDomains({
+        ParseResults: true,
+        ResponseHandler: function (r) {
+          for (var i = 0, lnt = (r.Domains || []).length; i < lnt; ++i) {
+            r.Domains[i].Value = decode(r.Domains[i].Value);
+            r.Domains[i].Text = decode(r.Domains[i].Text);
+          }
+
+          // that.initialize(r.Domains || []);
+          dispatch(setOrgDomainsAction(r.Domains));
+          setOneStepToInit(true);
+        },
+      });
+    }
+  }, [preinitDone]);
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://www.google.com/recaptcha/api.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [oneStepToInitDone]);
 
   return (
-    <Maintainer>
-      <WavyBackground />
-      <Container>
-        <Logo />
-        <Box smallScreen={isTabletOrMobile}>
-          {/* currenRoute handles custom route between login screens */}
-          {/* {currentRoute.includes('login') ? <LoginControl /> : <Signup />} */}
-          <Email />
-          <Password />
-          <Description />
+    <Maintainer className="small-12 medium-12 large-12 row">
+      <BackgroundImage />
 
-          <NavigateButton />
-          <ForgotPassword />
-          <Return />
-          <ContinueWithGoogle />
-          <CreateAccountButton />
-        </Box>
+      <div className="small-1 medium-2 large-4" />
+      <Container className="small-10 medium-8 large-4">
+        <Logo />
+
+        {oneStepToInitDone ? (
+          <Box>
+            <Title />
+            <Email />
+            <NameFamily />
+            <Password />
+            <OrgDomains />
+            <PasswordValidation />
+            <Description />
+            <VerificationCode />
+            <ResendCode />
+            <NavigationButton />
+            <ForgotPassword />
+            <Return />
+            <ContinueWithGoogle />
+            <CreateAccountButton />
+          </Box>
+        ) : (
+          <Center>
+            <Loader />
+          </Center>
+        )}
       </Container>
+      <div className="small-1 medium-2 large-4" />
     </Maintainer>
   );
 };
 
 export default Login;
-
-const Maintainer = styled.div`
-  background-color: rgb(42, 56, 143);
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  z-index: 4;
-  top: 100vw;
-`;
-const Box = styled.div`
-  display: flex;
-  width: ${({ smallScreen }) => (smallScreen ? '90vw' : '31vw')};
-  height: ${({ smallScreen }) => (smallScreen ? '90vh' : '83vh')};
-  padding-top: 2.6rem;
-
-  max-height: 80%;
-  background-color: white;
-  border-radius: 0.8rem;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  transition: all 1s;
-`;
