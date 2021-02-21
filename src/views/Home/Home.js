03e02f83e02d51633ee55f55d41ed77d753db12d
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-02-21 11:49:03 +0330

    Modify home page loading

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,26 +1,22 @@
-import { useEffect } from 'react';
+import { createRef } from 'react';
 import useScript from 'hooks/useScript';
 import { isEmpty } from 'helpers/helpers';
 
 const Home = (props) => {
+  const node = createRef();
+
   useScript(
     'pageLoadScripts/LoadHome/LoadHome.js',
     'LoadHome.js',
     (home) => {
-      !isEmpty(home) && window.loadHome(home);
+      !isEmpty(home) && !node.current.firstChild && window.loadHome(home);
     },
     props.route
   );
 
-  useEffect(() => {
-    const element = document.querySelector('#homeArea');
-    return () => {
-      element.innerHTML = '';
-    };
-  }, [props.route]);
-
   return (
     <div
+      ref={node}
       id="homeArea"
       className="small-12 medium-12 large-12 row align-center"
       style={{ margin: '0rem' }}></div>
