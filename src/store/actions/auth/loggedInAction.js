8502163fb1af41fc57e411e2e04e8e12539912c4
Ali Kargar <aligol20@gmail.com>
2021-02-24 07:27:25 +0330

    added steps after login

---
+++ src/store/actions/auth/loggedInAction.js
@@ -0,0 +1,45 @@
+/**
+ * A step after  the successful login.
+ */
+import { loginSlice } from '../../reducers/loginReducer';
+import { decode } from 'js-base64';
+
+const { loginSuccess, showLastLogins } = loginSlice.actions;
+const { GlobalUtilities, RVDic, RVAPI } = window;
+/**
+ * After successful login, this action will be called to completing the signing-in procedure.
+ * @param {Object} result - The response of backend after successful login.
+ */
+const loggedInAction = (result) => (dispatch, getState) => {
+  const { login } = getState();
+  const { AuthCookie } = result;
+
+  // The following steps are necessary to be done
+  window.isAuthenticated = true;
+  RVAPI.LoggedIn();
+  GlobalUtilities.set_auth_cookie(AuthCookie);
+
+  // It's needed for pure js, maybe removed in future.
+  const callback = () => {
+    window.location.href = login.Options.ReturnURL || window.location.href;
+  };
+
+  const msg = result.LoginMessage ? decode(result.LoginMessage) : null;
+
+  if (login.Options.ReloadAfterLogin) {
+    msg =
+      (msg || RVDic.MSG.LoggedInSuccessfully) +
+      '. ' +
+      RVDic.Confirms.DoYouWantToRefreshThePage;
+    GlobalUtilities.confirm(msg, function (r) {
+      if (r) callback();
+    });
+  } else {
+    if ((result.LastLogins || []).length > 0)
+      dispatch(showLastLogins({ message: msg, lastLogins: result.LastLogins }));
+    else if (msg) alert(msg, { Timeout: 10000 }, callback);
+    else callback();
+  }
+  dispatch(loginSuccess(result));
+};
+export default loggedInAction;
