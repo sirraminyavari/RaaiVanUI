cd47d38284e683c736d1f879537e6e311a65ad07
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-02-25 01:09:07 +0330

    Add jsdoc to Avatar component

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -4,7 +4,21 @@ import AvatarIcon from 'components/Icons/UserIcon/User';
 import * as Styled from './Avatar.styles';
 import { getURL } from 'helpers/helpers';
 
-const Avatar = ({ radius = 40, userImage, linkTo = getURL('User') }) => {
+/**
+ * @typedef PropType
+ * @type {Object}
+ * @property {number} [radius] - The radius of the avatar's circule.
+ * @property {string} userImage - User's image for avatar.
+ * @property {string} [linkTo] - The path to the user's profile page.
+ */
+
+/**
+ *  @description Renders an avatar component.
+ * @component
+ * @param {PropType} props -Props that pass to avatar.
+ */
+const Avatar = (props) => {
+  const { radius, userImage, linkTo } = props;
   return (
     <Styled.AvatarContainer as={Link} to={linkTo}>
       {userImage ? (
@@ -20,6 +34,11 @@ Avatar.propTypes = {
   radius: PropTypes.number,
   userImage: PropTypes.string,
   linkTo: PropTypes.string,
+};
+
+Avatar.defaultProps = {
+  radius: 40,
+  linkTo: getURL('User'),
 };
 
 export default Avatar;
