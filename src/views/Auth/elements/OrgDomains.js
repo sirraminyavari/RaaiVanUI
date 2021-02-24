cf5b44c83be41d0ae520ba17b162c49e92d97efd
Ali Kargar <aligol20@gmail.com>
2021-02-24 07:43:14 +0330

    add set to redux state by click

--- src/views/Auth/elements/OrgDomains.js
+++ src/views/Auth/elements/OrgDomains.js
@@ -3,23 +3,28 @@
  */
 import AnimatedDropDownList from 'components/DropDownList/AnimatedDropDownList';
 import { SIGN_IN, SIGN_IN_COLLAPSED } from 'const/LoginRoutes';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import setSelectedOrgDomainAction from 'store/actions/auth/setSelectedOrgDomainAction';
 import { UpToDownAnimate } from './Animate.style';
 
 const { RVDic } = window;
 
 const OrgDomains = () => {
-  const { orgDomains, currentRoute } = useSelector((state) => ({
+  const dispatch = useDispatch();
+  const { orgDomains, currentRoute, selectedDomain } = useSelector((state) => ({
     orgDomains: state.login.orgDomains,
     currentRoute: state.login.currentRoute,
+    selectedDomain: state.login.selectedDomain,
   }));
+
   // const orgDomains = ['one', 'two', 'three', 'four'];
   /**
    * According to 'currentRoute'
    * this function decides to return true or false.
    */
   const isVisible = () => {
+    console.log(orgDomains, 'org *** org ***');
     switch (currentRoute) {
       case SIGN_IN:
       case SIGN_IN_COLLAPSED:
@@ -30,18 +35,20 @@ const OrgDomains = () => {
   };
   /**
    * By clicking the list item will fire.
-   * @param {object} value - object of selected orgDomain
+   * @param {number} index - index of selected orgDomain
+   * @param {String} title - title of selected orgDomain
    */
-  const onSelectItem = (value) => {
-    console.log(value, 'on select');
+  const onSelectItem = (title, index) => {
+    console.log(orgDomains[index], index, 'on select');
+    dispatch(setSelectedOrgDomainAction(orgDomains[index]));
   };
 
   return (
     <UpToDownAnimate isVisible={isVisible()} style={{ zIndex: 10 }}>
       <AnimatedDropDownList
-        label={RVDic.DomainSelect}
+        label={selectedDomain ? selectedDomain?.Title : RVDic.DomainSelect}
         onSelectItem={onSelectItem}
-        list={orgDomains}
+        list={orgDomains.map((x, index) => x?.Title)}
       />
     </UpToDownAnimate>
   );
