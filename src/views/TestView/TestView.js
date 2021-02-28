f7cc24ae5b812e851badf679905253ea5978fb8b
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-03-01 00:12:32 +0330

    Init CKEditor with test in view

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -1,26 +1,37 @@
 import { useState, useEffect } from 'react';
-// import CustomCalendar from 'components/CustomCalendar/CustomCalendar';
-import CustomDatePicker from 'components/CustomDatePicker/CustomDatePicker';
-import { getToday, getLanguageDigits } from 'helpers/helpers';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const TestView = () => {
-  const [selectedDay, setSelectedDay] = useState(getToday('fa'));
+  const [data, setData] = useState('<p>Hello from CKEditor 5!</p>');
 
   useEffect(() => {
-    console.log(selectedDay);
-  }, [selectedDay]);
+    console.log(data);
+  }, [data]);
 
   return (
-    <CustomDatePicker
-      minimumDate={getToday('fa')}
-      maximumDate={{ ...getToday('fa'), day: getToday('fa').day + 7 }}
-      value={selectedDay}
-      onChange={setSelectedDay}
-      disabledDays={[{ ...getToday('fa'), day: getToday('fa').day + 3 }]}
-      onDisabledDayError={(disDay) =>
-        alert(`Day: ${getLanguageDigits('fa', disDay.day)} has been clicked`)
-      }
-    />
+    <div>
+      <h2>Using CKEditor 5 build in React</h2>
+      <CKEditor
+        editor={ClassicEditor}
+        data={data}
+        onReady={(editor) => {
+          // You can store the "editor" and use when it is needed.
+          console.log('Editor is ready to use!', editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setData(data);
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event, editor) => {
+          console.log('Blur.', editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log('Focus.', editor);
+        }}
+      />
+    </div>
   );
 };
 
