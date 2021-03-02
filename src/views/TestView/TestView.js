98a41d48065a8aa918d1e0e3aff8e3991868c43e
Rasoul678 <h.rostami.r@gmail.com>
2021-03-02 13:04:52 +0330

    :package: Add custom classic editor with our own npm package

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from '@rasoul678/ckeditor5-custom-classic';
 
 const TestView = () => {
   const [data, setData] = useState('<p>Hello from CKEditor 5!</p>');
