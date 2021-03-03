f9bbf742335ad553895edbeb7e299342b234265b
Ramin <sir.raminyavari@gmail.com>
2021-03-03 19:59:24 +0330

    added ckeditor5

--- src/components/RVEditor/RVEditor.js
+++ src/components/RVEditor/RVEditor.js
@@ -1,116 +1,33 @@
 import React from 'react';
+import useLoadFiles from '../../hooks/useLoadFiles';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import Editor from '@ckeditor/ckeditor5-editor-balloon';
-
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import Autolink from '@ckeditor/ckeditor5-link/src/autolink.js';
-import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
-import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar.js';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
-import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
-import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
-import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
-import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
-import Image from '@ckeditor/ckeditor5-image/src/image.js';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
-import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import List from '@ckeditor/ckeditor5-list/src/list.js';
-import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle.js';
-import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
-import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
-import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
-import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js';
-import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
-import SpecialCharactersArrows from '@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows.js';
-import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js';
-import SpecialCharactersLatin from '@ckeditor/ckeditor5-special-characters/src/specialcharacterslatin.js';
-import SpecialCharactersMathematical from '@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical.js';
-import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js';
-import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
-import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
-import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
-import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
-import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 
 const { RV_Lang } = window;
 
 const RVEditor = () => {
-  const xx = [
-    Mention /*function (editor) { that.mention_customization(editor); },*/ /*uploadPlugin*/,
-    ,
-    Alignment,
-    Autoformat,
-    Autolink,
-    BlockQuote,
-    BlockToolbar,
-    Bold,
-    Code,
-    CodeBlock,
-    Essentials,
-    FontColor,
-    FontBackgroundColor,
-    Heading,
-    HorizontalLine,
-    Image,
-    ImageCaption,
-    ImageResize,
-    ImageStyle,
-    ImageToolbar,
-    ImageUpload,
-    SimpleUploadAdapter /*"FontFamily", "FontSize",*/,
-    Indent,
-    IndentBlock,
-    Italic,
-    Link,
-    List,
-    ListStyle,
-    /*"Markdown",*/ MediaEmbed,
-    MediaEmbedToolbar,
-    Paragraph,
-    PasteFromOffice,
-    RemoveFormat,
-    SpecialCharacters,
-    SpecialCharactersArrows,
-    SpecialCharactersEssentials,
-    SpecialCharactersLatin,
-    SpecialCharactersMathematical,
-    SpecialCharactersText,
-    Strikethrough,
-    Subscript,
-    Superscript,
-    Table,
-    TableCellProperties,
-    TableProperties,
-    TableToolbar,
-    TextTransformation,
-    Underline,
-  ];
+  const loaded = useLoadFiles([
+    "CKEditor5/ckeditor.js",
+    "CKEditor5/translations/fa.js"
+  ]);
+
+  if(!loaded) return (<></>);
+
+  const { BalloonBlockEditor } = window;
+
+  let specialCharPlugins = BalloonBlockEditor.builtinPlugins.filter(f => !f.pluginName);
 
   const editorConfig = {
     language: RV_Lang,
-    plugins: [Mention],
+    plugins: [
+      "Mention", /*function (editor) { that.mention_customization(editor); }, uploadPlugin,*/
+      "Alignment", "Autoformat", "AutoLink", "BlockQuote", "BlockToolbar", "Bold", "Code", "CodeBlock", "Essentials",
+      "FontColor", "FontBackgroundColor", "Heading", "HorizontalLine", "Image", "ImageCaption", "ImageResize",
+      "ImageStyle", "ImageToolbar", "ImageUpload", "SimpleUploadAdapter",
+      "Indent", "IndentBlock", "Italic", "Link", "List", "ListStyle", "MediaEmbed", "MediaEmbedToolbar",
+      "Paragraph", "PasteFromOffice", "RemoveFormat", "SpecialCharacters", "Strikethrough", "Subscript",
+      "Superscript", "Table", "TableCellProperties", "TableProperties", "TableToolbar", "TextTransformation",
+      "Underline"
+    ].concat(specialCharPlugins),
     toolbar: {
       items: [
         'bold',
@@ -180,8 +97,9 @@ const RVEditor = () => {
 
   return (
     <CKEditor
-      editor={Editor.BalloonEditor}
+      editor={BalloonBlockEditor}
       config={editorConfig}
+      data="<p>ramin yavari</p>"
       onReady={(editor) => {
         // You can store the "editor" and use when it is needed.
         console.log('Editor is ready to use!', editor);
