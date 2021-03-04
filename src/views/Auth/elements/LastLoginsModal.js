9fa8e160bb1c6a0294d1802d7ddfeb540482b89a
Ali Kargar <aligol20@gmail.com>
2021-03-04 17:26:33 +0330

    fix bug

--- src/views/Auth/elements/LastLoginsModal.js
+++ src/views/Auth/elements/LastLoginsModal.js
@@ -7,6 +7,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import setIsAthunticatedAction from 'store/actions/auth/setIsAthunticatedAction';
+import { decode } from 'js-base64';
 
 /**
  * By signing in the user, this modal will be shown
@@ -14,9 +15,10 @@ import setIsAthunticatedAction from 'store/actions/auth/setIsAthunticatedAction'
 const LastLoginsModal = () => {
   const dispatch = useDispatch();
 
-  const { lastLoginModal, lastLogins } = useSelector((state) => ({
+  const { lastLoginModal, lastLogins, loginMessage } = useSelector((state) => ({
     lastLoginModal: state.auth.lastLoginModal,
     lastLogins: state.auth.lastLogins,
+    loginMessage: state.auth.auth.LoginMessage,
   }));
   /**
    * By clicking close button, will fire.
@@ -26,22 +28,26 @@ const LastLoginsModal = () => {
   };
 
   return (
-    <Modal onClose={onClose} contentWidth={'90%'} show={lastLoginModal}>
-      <Row style={{ marginBottom: '3px' }}>
-        <div>{'  '} </div>
-        <RowItem flex={1}>{'نوع اقدام'}</RowItem>
-        <RowItem flex={2}>{'زمان'}</RowItem>
-        <RowItem flex={2}>{'آدرس آی پی'}</RowItem>
-      </Row>
-      {lastLogins?.length > 0 &&
-        lastLogins.map((x, index) => (
-          <Row key={index}>
-            <div>{index + 1}.</div>
-            <RowItem flex={1}>{x?.Action}</RowItem>
-            <RowItem flex={2}>{x?.Date}</RowItem>
-            <RowItem flex={2}>{x?.HostAddress}</RowItem>
-          </Row>
-        ))}
+    <Modal onClose={onClose} contentWidth={'50%'} show={lastLoginModal}>
+      {console.log(loginMessage, 'loginMessage')}
+      <Container>
+        <Message>{decode(loginMessage)} </Message>
+
+        <Row style={{ marginBottom: '3px' }}>
+          <RowItem flex={1}>{'نوع اقدام'}</RowItem>
+          <RowItem flex={2}>{'زمان'}</RowItem>
+          <RowItem flex={2}>{'آدرس آی پی'}</RowItem>
+        </Row>
+        {lastLogins?.length > 0 &&
+          lastLogins.map((x, index) => (
+            <Row key={index}>
+              <div>{index + 1}.</div>
+              <RowItem flex={1}>{x?.Action}</RowItem>
+              <RowItem flex={2}>{x?.Date}</RowItem>
+              <RowItem flex={2}>{x?.HostAddress}</RowItem>
+            </Row>
+          ))}
+      </Container>
     </Modal>
   );
 };
@@ -52,6 +58,7 @@ const Row = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
 `;
 const RowItem = styled.div`
   display: flex;
@@ -62,4 +69,16 @@ const RowItem = styled.div`
   margin: 3px;
   align-items: center;
   justify-content: center;
+`;
+const Message = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-bottom: 1rem;
+`;
+const Container = styled.div`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
 `;
