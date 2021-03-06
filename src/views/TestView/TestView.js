d3a3431208f8dd9d52cbb7553881a7edf1105bcc
Rasoul678 <h.rostami.r@gmail.com>
2021-03-06 09:44:45 +0330

    Add balloon block editor

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@rasoul678/ckeditor5-custom-classic';
+// import ClassicEditor from '@rasoul678/ckeditor5-custom-classic';
+import BalloonBlockEditor from '@rasoul678/ckeditor5-custom-balloon-block';
 
 const TestView = () => {
   const [data, setData] = useState('<p>Hello from CKEditor 5!</p>');
@@ -10,10 +11,10 @@ const TestView = () => {
   }, [data]);
 
   return (
-    <div>
+    <div style={{ padding: '50px' }}>
       <h2>Using CKEditor 5 build in React</h2>
       <CKEditor
-        editor={ClassicEditor}
+        editor={BalloonBlockEditor}
         data={data}
         onReady={(editor) => {
           // You can store the "editor" and use when it is needed.
