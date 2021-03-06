f4c228c700768697659397fb099f940e8cab37ee
Rasoul678 <h.rostami.r@gmail.com>
2021-03-06 18:25:00 +0330

    Add embed.js to test view

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 // import ClassicEditor from '@rasoul678/ckeditor5-custom-classic';
 import BalloonBlockEditor from '@rasoul678/ckeditor5-custom-balloon-block';
+import Helmet from 'react-helmet';
 
 const TestView = () => {
   const [data, setData] = useState('<p>Hello from CKEditor 5!</p>');
@@ -11,28 +12,34 @@ const TestView = () => {
   }, [data]);
 
   return (
-    <div style={{ padding: '50px' }}>
-      <h2>Using CKEditor 5 build in React</h2>
-      <CKEditor
-        editor={BalloonBlockEditor}
-        data={data}
-        onReady={(editor) => {
-          // You can store the "editor" and use when it is needed.
-          console.log('Editor is ready to use!', editor);
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          setData(data);
-          console.log({ event, editor, data });
-        }}
-        onBlur={(event, editor) => {
-          console.log('Blur.', editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log('Focus.', editor);
-        }}
-      />
-    </div>
+    <>
+      <Helmet>
+        <script async charset="utf-8" src="//cdn.iframe.ly/embed.js"></script>
+      </Helmet>
+      <div style={{ padding: '50px' }}>
+        <h2>Using CKEditor 5 build in React</h2>
+        <CKEditor
+          editor={BalloonBlockEditor}
+          data={data}
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            setData(data);
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: data }}></div>
+    </>
   );
 };
 
