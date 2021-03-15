e9d2280b0b22b5b59758f5a59e6a10228f1924c9
Ali Kargar <aligol20@gmail.com>
2021-03-15 15:51:24 +0330

    deleted

--- src/utils/Valiation/MobileNumberValidator.js
+++
@@ -1,8 +0,0 @@
-/**
- * Checks inputted number with regex.
- * @param {Number} number - mobile number
- */
-const MobileNumberValidator = (number) => {
-  return /(^(09)[1][1-9]\d{7}$)|(^(09)[3][12456]\d{7}$)/.test(number);
-};
-export default MobileNumberValidator;
--- src/utils/Valiation/PersianValidator.js
+++
@@ -1,8 +0,0 @@
-/**
- * Checks inputted name is persian
- */
-import persianRex from 'persian-rex';
-const PersianValidator = (name) => {
-  return persianRex.letter.test(name);
-};
-export default PersianValidator;
