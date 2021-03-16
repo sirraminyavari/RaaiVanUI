826afe4630b1680e8a0fa34b74ba5c01702c3513
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-03-16 09:50:44 +0330

    Fix: Page load duplication

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import { useRef } from 'react';
 import useScript from 'hooks/useScript';
 import { isEmpty } from 'helpers/helpers';
 
 const Profile = (props) => {
+  const node = useRef();
+
   useScript(
     'pageLoadScripts/LoadProfile/LoadProfile.js',
     'LoadProfile.js',
     (user) => {
-      !isEmpty(user) && window.loadProfile(user);
+      !isEmpty(user) && !node.current.firstChild && window.loadProfile(user);
     },
     props.route
   );
@@ -19,6 +21,7 @@ const Profile = (props) => {
         style={styles.coverContainer}></div>
 
       <div
+        ref={node}
         id="profileArea"
         className="small-12 medium-12 large-12"
         style={{ padding: '0vw 6vw', marginBottom: '8rem' }}></div>
