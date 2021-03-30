1f38527119214e430641f6291949374c145cce15
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-03-30 12:23:02 +0430

    test: modify cypress test

--- cypress/integration/raaivan/cypress.spec.js
+++ cypress/integration/raaivan/cypress.spec.js
@@ -1,9 +1,11 @@
-describe('Cypress', () => {
+describe('Cypress is working', () => {
   it('is working', () => {
     expect(true).to.equal(true);
   });
+});
 
-  it('visits the app', () => {
-    cy.visit('/');
+describe('The Login Page', () => {
+  it('visits the login page', () => {
+    cy.visit('/auth/login');
   });
 });
