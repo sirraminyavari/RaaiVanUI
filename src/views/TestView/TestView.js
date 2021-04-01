10d8a9ab483941bf9d751eee7f7e4b1494705c34
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-04-01 20:00:51 +0430

    feat: add page wrapper

--- src/views/TestView/TestView.js
+++ src/views/TestView/TestView.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import CustomDatePicker from 'components/CustomDatePicker/CustomDatePicker';
+import PageWrapper from 'components/PageWrapper/PageWrapper';
 
 const TestView = () => {
   const [date, setDate] = useState(null);
@@ -13,17 +14,19 @@ const TestView = () => {
   }, [date]);
 
   return (
-    <div style={{ padding: '5px', width: '10rem', margin: '100px' }}>
-      <CustomDatePicker
-        type="jalali"
-        mode="input"
-        value={date}
-        range={true}
-        clearButton={true}
-        size="medium"
-        onDateSelect={handleDateSelect}
-      />
-    </div>
+    <PageWrapper title="Raaivan | development">
+      <div style={{ padding: '5px', width: '10rem', margin: '100px' }}>
+        <CustomDatePicker
+          type="jalali"
+          mode="input"
+          value={date}
+          range={true}
+          clearButton={true}
+          size="medium"
+          onDateSelect={handleDateSelect}
+        />
+      </div>
+    </PageWrapper>
   );
 };
 
