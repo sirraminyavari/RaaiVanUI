b1e76438851a50308bec8d77106e12209af4107d
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-04-11 12:16:12 +0430

    refactor: make some seperation in index.js

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import PrivateRoute from 'utils/RouteHandler/PrivateRoute';
+import PublicRoute from 'utils/RouteHandler/PublicRoute';
+import StoreProvider from 'store/StoreProvider';
+import ErrorBoundry from 'components/ErrorBoundry/ErrorBoundry';
+import LogoLoader from 'components/Loaders/LogoLoader/LogoLoader';
+import AuthView from 'views/Auth/AuthView';
+import 'assets/css/index.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ScrollToTop from 'components/ScrollToTop/ScrollToTop';
+
+const { GlobalUtilities, RV_RTL, RV_Float, RV_RevFloat } = window;
+
+const MainLayout = lazy(() =>
+  import(/* webpackChunkName: "layout-main"*/ 'layouts/Main')
+);
+window._alert = window.alert;
+window.alert = function (txt, type = '') {
+  toast(txt, {
+    type,
+    position: RV_Float === 'right' ? 'bottom-right' : 'bottom-left',
+  });
+};
+
+const App = () => {
+  return (
+    <>
+      <ToastContainer />
+
+      <StoreProvider>
+        <ErrorBoundry>
+          <Suspense fallback={<LogoLoader size={10} />}>
+            <Router>
+              <ScrollToTop />
+              <Switch>
+                <PublicRoute path="/auth" component={AuthView} />
+                <PrivateRoute path="/" component={MainLayout} />
+              </Switch>
+            </Router>
+          </Suspense>
+        </ErrorBoundry>
+      </StoreProvider>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,47 +1,10 @@
-import { Suspense, lazy, StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import PrivateRoute from 'utils/RouteHandler/PrivateRoute';
-import PublicRoute from 'utils/RouteHandler/PublicRoute';
-import StoreProvider from 'store/StoreProvider';
-import ErrorBoundry from 'components/ErrorBoundry/ErrorBoundry';
-import LogoLoader from 'components/Loaders/LogoLoader/LogoLoader';
-import AuthView from 'views/Auth/AuthView';
-import 'assets/css/index.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import ScrollToTop from 'components/ScrollToTop/ScrollToTop';
-
-const { GlobalUtilities, RV_RTL, RV_Float, RV_RevFloat } = window;
-
-const MainLayout = lazy(() =>
-  import(/* webpackChunkName: "layout-main"*/ 'layouts/Main')
-);
-window._alert = window.alert;
-window.alert = function (txt, type = '') {
-  toast(txt, {
-    type,
-    position: RV_Float === 'right' ? 'bottom-right' : 'bottom-left',
-  });
-};
+import App from './App';
 
 render(
   <StrictMode>
-    <ToastContainer />
-
-    <StoreProvider>
-      <ErrorBoundry>
-        <Suspense fallback={<LogoLoader size={10} />}>
-          <Router>
-            <ScrollToTop />
-            <Switch>
-              <PublicRoute path="/auth" component={AuthView} />
-              <PrivateRoute path="/" component={MainLayout} />
-            </Switch>
-          </Router>
-        </Suspense>
-      </ErrorBoundry>
-    </StoreProvider>
+    <App />
   </StrictMode>,
   document.getElementById('root')
 );
