ad7b3287e6899301b968773bdd0b21f45bc67489
Ali Kargar <aligol20@gmail.com>
2021-04-26 15:54:34 +0430

    A component for viewing the subject's item.

---
+++ src/components/SubjectItem/screen/SubjectItem.style.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import DimensionHelper from 'utils/DimensionHelper/DimensionHelper';
+
+export const Container = styled.div`
+  border-radius: 0.75rem;
+  display: flex;
+  height: ${() => (DimensionHelper().isTabletOrMobile ? '13rem' : '7.5rem')};
+  align-items: center;
+  background-color: white;
+  margin: 3px;
+  justify-content: space-between;
+  border: 0.05rem solid #bac9dc;
+  max-width: 100%;
+  width: 100%;
+  transition: max-width 0.5s;
+`;
+export const RightSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 7.5rem;
+  height: 100%;
+  border-radius: 0.5rem;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+export const Divider = styled.div`
+  height: 80%;
+  width: 0.05rem;
+  background-color: #bac9dc;
+  /* border: 0.5px solid #bac9dc; */
+  opacity: 1;
+`;
+export const Root = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
