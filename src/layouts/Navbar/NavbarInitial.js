2fae2bd3e8948827e048201a29b2977bbdb1c7a3
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-06-28 15:24:12 +0430

    feat: add help me route

--- src/layouts/Navbar/NavbarInitial.js
+++ src/layouts/Navbar/NavbarInitial.js
@@ -1,32 +1,46 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { createSelector } from 'reselect';
 import * as Styled from './Navbar.styles';
 import Logo_Fa from 'assets/images/cliqmind_logo_white.svg';
 import Logo_En from 'assets/images/cliqmind_logo_white_en.svg';
 import QuestionIcon from 'components/Icons/QuestionIcon/QuestionIcon';
+import TeamIcon from 'components/Icons/TeamIcon/TeamIcon';
 import ExitIcon from 'components/Icons/ExitIcon/ExitIcon';
 import useWindow from 'hooks/useWindowContext';
 import logoutAction from 'store/actions/auth/logoutAction';
+import { getURL } from 'helpers/helpers';
+
+const selectActivePath = createSelector(
+  (state) => state.theme,
+  (theme) => theme.activePath
+);
 
 const NavbarInitial = () => {
   const dispatch = useDispatch();
   const { RVGlobal, RV_RTL } = useWindow();
   const isSaas = RVGlobal.SAASBasedMultiTenancy;
+  const activePath = useSelector(selectActivePath);
 
   //! Logs user out from application.
   const handleLogout = () => {
     dispatch(logoutAction());
   };
 
+  const isHelpmeActive = activePath === getURL('Help');
+
+  const linkTo = isHelpmeActive ? getURL('Applications') : getURL('Help');
+
   return (
     <Styled.NavbarContainer>
       <img
         src={isSaas ? (RV_RTL ? Logo_Fa : Logo_En) : RVGlobal.LogoURL}
         width={isSaas ? '120' : '60'}
-        alt="logo-icon"
+        alt="cliqmind-logo"
       />
       <Styled.ExitAndHelpWrapper>
-        <Styled.QuestionIconWrapper>
-          <QuestionIcon size={22} />
+        <Styled.QuestionIconWrapper as={Link} to={linkTo}>
+          {isHelpmeActive ? <TeamIcon size={27} /> : <QuestionIcon size={22} />}
         </Styled.QuestionIconWrapper>
         <Styled.ExitIconWrapper onClick={handleLogout}>
           <ExitIcon size={30} />
