6435409e4969fa2fd500178df1247aee6045be0f
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-07-17 15:35:18 +0430

    feat: add check route to auth view

--- src/routes/AuthRoutes/Auth.routes.js
+++ src/routes/AuthRoutes/Auth.routes.js
@@ -1,48 +1,64 @@
 /**
  * Returns a list of authentication routes.
  */
-// import SignIn from 'views/Auth/items/SignIn';
+import SignIn from 'views/Auth/items/SignIn';
 import SignUp from 'views/Auth/items/SignUp';
 import VerifyingCode from 'views/Auth/items/VerifyingCode';
 import ForgotPassword from 'views/Auth/items/ForgotPassword';
 import ResetPasswordAddress from 'views/Auth/items/ResetPasswordAddress';
 import VerifyingResetPassword from 'views/Auth/items/VerifyingResetPassword';
 import ResetPassword from 'views/Auth/items/ResetPassword';
+import {
+  FORGOT_PASS_NAME,
+  FORGOT_PASS_PATH,
+  LOGIN_NAME,
+  LOGIN_PATH,
+  REGISTER_NAME,
+  REGISTER_PATH,
+  RESET_PASS_ADDRESS_NAME,
+  RESET_PASS_ADDRESS_PATH,
+  RESET_PASS_NAME,
+  RESET_PASS_PATH,
+  VERIFICATION_NAME,
+  VERIFICATION_PATH,
+  VERIFY_RESET_NAME,
+  VERIFY_RESET_PATH,
+} from 'constant/constants';
 
 const routes = [
-  // {
-  //   path: '/auth/login',
-  //   name: 'login',
-  //   component: SignIn,
-  // },
   {
-    path: '/auth/register',
-    name: 'register',
+    path: LOGIN_PATH,
+    name: LOGIN_NAME,
+    component: SignIn,
+  },
+  {
+    path: REGISTER_PATH,
+    name: REGISTER_NAME,
     component: SignUp,
   },
   {
-    path: '/auth/verificationCode',
-    name: 'verificationCode',
+    path: VERIFICATION_PATH,
+    name: VERIFICATION_NAME,
     component: VerifyingCode,
   },
   {
-    path: '/auth/verifyingResetPassword',
-    name: 'verifyingResetPassword',
+    path: VERIFY_RESET_PATH,
+    name: VERIFY_RESET_NAME,
     component: VerifyingResetPassword,
   },
   {
-    path: '/auth/forgotPassword',
-    name: 'forgotPassword',
+    path: FORGOT_PASS_PATH,
+    name: FORGOT_PASS_NAME,
     component: ForgotPassword,
   },
   {
-    path: '/auth/resetPasswordAddress',
-    name: 'resetPasswordAddress',
+    path: RESET_PASS_ADDRESS_PATH,
+    name: RESET_PASS_ADDRESS_NAME,
     component: ResetPasswordAddress,
   },
   {
-    path: '/auth/resetPassword',
-    name: 'resetPassword',
+    path: RESET_PASS_PATH,
+    name: RESET_PASS_NAME,
     component: ResetPassword,
   },
 ];
