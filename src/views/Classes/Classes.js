ec2477f9a1d15aa4471f3999956a14a9e7ee7965
sirraminyavari <sir.raminyavari@gmail.com>
2021-07-19 13:01:18 +0430

    modified Classes.js

--- src/views/Classes/Classes.js
+++ src/views/Classes/Classes.js
@@ -14,7 +14,7 @@ const AdvancedSearchView = (props) => {
   useEffect(() => {
     dispatch(toggleActivation());
   }, [props.route]);
-  console.log(route, 'route');
+
   return (
     <Fragment>
       <AdvanceSearch
