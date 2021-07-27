9fe2d42e8683e46216cfc6e8db3f40f292ac39fd
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-07-27 12:40:06 +0430

    feat: add json animation for loading

--- src/components/Loaders/LogoLoader/LogoLoader.js
+++ src/components/Loaders/LogoLoader/LogoLoader.js
@@ -1,14 +1,24 @@
-import cliqmindLoader from 'assets/images/loading-new.gif';
+// import cliqmindLoader from 'assets/images/loading-new.gif';
 import inprogressLoader from 'assets/images/loading_progress_bar.gif';
 import * as Styled from './LogoLoader.styles';
+import LogoLottieMaker from 'components/LottieMaker/LottieMaker';
+import animationData from 'assets/lotties/loading.json';
 
 const LogoLoader = ({ style }) => {
   const isSaas = (window.RVGlobal || {}).SAASBasedMultiTenancy;
-  const loader = isSaas ? cliqmindLoader : inprogressLoader;
 
   return (
     <Styled.LogoContainer style={style}>
-      <Styled.Image src={loader} alt="cliqmind-logo" />
+      {isSaas ? (
+        <LogoLottieMaker
+          animationJSON={animationData}
+          loop
+          autoplay
+          width="5rem"
+        />
+      ) : (
+        <Styled.Image src={inprogressLoader} alt="cliqmind-logo" />
+      )}
     </Styled.LogoContainer>
   );
 };
---
+++ src/components/LottieMaker/LottieMaker.js
@@ -0,0 +1,50 @@
+import Lottie from 'react-lottie';
+import PropTypes from 'prop-types';
+
+/**
+ * @typedef PropType
+ * @type {Object}
+ * @property {object} animationJSON - The JSON file for animation.
+ * @property {boolean} [loop] - If exists, animation loop is true.
+ * @property {boolean} [autoplay] - If exists, animation autoplay is true.
+ * @property {string | number} height - Width of animation.
+ * @property {string | number} [width] - Height of animation.
+ */
+
+/**
+ *  @description Renders a LottieMaker component.
+ * @component
+ * @param {PropType} props -Props that pass to LottieMaker.
+ */
+const LottieMaker = (props) => {
+  const { animationJSON, loop, autoplay, height, width } = props;
+
+  const defaultOptions = {
+    loop: !!loop,
+    autoplay: !!autoplay,
+    animationData: animationJSON,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return <Lottie options={defaultOptions} height={height} width={width} />;
+};
+
+LottieMaker.propTypes = {
+  animationJSON: PropTypes.object.isRequired,
+  loop: PropTypes.bool,
+  autoplay: PropTypes.bool,
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+LottieMaker.defaultProps = {
+  loop: true,
+  autoplay: true,
+  width: '5rem',
+};
+
+LottieMaker.displayName = 'LottieMakerComponent';
+
+export default LottieMaker;
