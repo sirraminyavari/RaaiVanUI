b3c8276626239f68e1ec5a09368ff6011ac12e78
Ali Kargar <aligol20@gmail.com>
2021-09-12 14:52:06 +0430

    fix NodeDetails problem

--- src/routes/MainRoutes/Main.routes.js
+++ src/routes/MainRoutes/Main.routes.js
@@ -45,7 +45,7 @@ const NewNode = lazy(() =>
   import(/* webpackChunkName: "new-node-view"*/ 'views/NewNode/NewNode')
 );
 const NodeView = lazy(() =>
-  import(/* webpackChunkName: "node-view"*/ 'views/Node/Node-view')
+  import(/* webpackChunkName: "node-view "*/ 'views/Node/Node-view')
 );
 const FormView = lazy(() =>
   import(/* webpackChunkName: "form-view"*/ 'views/Form/Form-view')
