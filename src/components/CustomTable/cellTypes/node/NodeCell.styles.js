1e5c7847d72c1e43c3561fc9e224ffc3c3d4f1e0
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2021-11-01 12:32:44 +0330

    feat: modify node, file and user cells update UI

--- src/components/CustomTable/cellTypes/node/NodeCell.styles.js
+++ src/components/CustomTable/cellTypes/node/NodeCell.styles.js
@@ -27,7 +27,8 @@ export const NodeCellContainer = styled.div`
   .table-node-cell-select-button {
     background-color: inherit;
     color: ${TCV_DEFAULT};
-    width: 9rem;
+    width: auto;
+    margin: 0 1rem;
     height: 1.8rem;
     border-radius: 1rem;
   }
@@ -128,6 +129,17 @@ export const SaveButtonHeading = styled(Heading)`
 export const EmptyCellView = styled.div`
   color: ${CV_DISTANT};
   width: 100%;
+  height: 2rem;
   text-align: start;
   padding-${RV_Float}: 0.7rem;
+`;
+
+export const ItemPickerWrapper = styled.div`
+  width: 100%;
+`;
+
+export const NodesListWrapper = styled.div`
+  width: 100%;
+  ${FLEX_CCC}
+  gap: 0.2rem;
 `;
