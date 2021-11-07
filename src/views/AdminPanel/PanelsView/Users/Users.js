22b8ee6a59caf01403afc9ad9b1a0aa65c20e8ef
m.khalafi <mehdi.khalafi@outlook.com>
2021-11-07 16:24:39 +0330

    branch set

--- src/views/AdminPanel/PanelsView/Users/Users.js
+++ src/views/AdminPanel/PanelsView/Users/Users.js
@@ -2,20 +2,22 @@ import useScript from 'hooks/useScript';
 import { isEmpty } from 'helpers/helpers';
 
 const Users = (props) => {
-  useScript(
-    'pageLoadScripts/LoadUsers/LoadUsers.js',
-    'LoadUsers.js',
-    (users) => {
-      !isEmpty(users) && window.loadUsers(users);
-    },
-    props.route
-  );
-  return (
-    <div
-      id="usersArea"
-      className="small-12 medium-12 large-12 row align-center"
-      style={{ marginBottom: '5rem', padding: '0 10vw 0 10vw' }}></div>
-  );
+  // useScript(
+  //   'pageLoadScripts/LoadUsers/LoadUsers.js',
+  //   'LoadUsers.js',
+  //   (users) => {
+  //     !isEmpty(users) && window.loadUsers(users);
+  //   },
+  //   props.route
+  // );
+  // return (
+  //   <div
+  //     id="usersArea"
+  //     className="small-12 medium-12 large-12 row align-center"
+  //     style={{ marginBottom: '5rem', padding: '0 10vw 0 10vw' }}></div>
+  // );
+
+  return <div>users...</div>;
 };
 
 export default Users;
