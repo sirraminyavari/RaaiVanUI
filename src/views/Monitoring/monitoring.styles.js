ac7c0f157974872fce785f4bae1ad710ddba9354
MaryamMaryam <m.danehvash2014@gmail.com>
2021-12-19 19:32:35 +0330

    monitoring

---
+++ src/views/Monitoring/monitoring.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border-radius: 0.35rem;
+  position: relative;
+  box-shadow: 1px 5px 15px #0000001f;
+  margin: 1.5rem;
+  padding: 1.5rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 1.5rem;
+  padding: 0.3rem;
+`;
