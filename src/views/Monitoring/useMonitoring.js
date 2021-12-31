775a5f0472294805d86a6c164cd562634b6843ae
MaryamMaryam <m.danehvash2014@gmail.com>
2021-12-31 12:43:29 +0330

    add img and lastactivity into table

--- src/views/Monitoring/useMonitoring.js
+++ src/views/Monitoring/useMonitoring.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
 import { GetApplicationsMonitoring } from 'apiHelper/apiFunctions';
+import { useEffect, useState } from 'react';
 
 const useAppMonitoring = (
   totalUsersCount = true,
