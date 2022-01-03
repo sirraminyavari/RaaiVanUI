fc2d512be121a65e98921cbdc227cd1f7d45b6c2
soren abedi <soren.abedi@gmail.com>
2022-01-03 22:13:40 +0330

    feat: :sparkles: create `DeleteConfirm` message component
    Signed-off-by: soren abedi <soren.abedi@gmail.com>

---
+++ src/components/Modal/Messages/DeleteConfirm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { TC_DEFAULT } from 'constant/Colors';
+import { CV_GRAY_DARK, CV_RED } from 'constant/CssVariables';
+
+const TextContainer = styled.p`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  font-size: 0.85rem;
+  margin-bottom: ${({ bottomMargin }) => (bottomMargin ? bottomMargin : '')};
+  color: ${({ color }) => (color ? color : 'unset')};
+  font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')};
+`;
+/**
+ *
+ * Delete Confirm Message component
+ *
+ * @param {object} props - DeleteConfirmMSG component props
+ * @param {string} [props.title=""] - Message title
+ * @param {JSX.Element} props.Icon - JSX icon component
+ * @param {string} [props.question=""] - Message question
+ * @param {string} [props.warning=""] - Message warning
+ *
+ * @return {JSX.Element} The fragment created.
+ */
+const DeleteConfirmMSG = ({ title, Icon, question, warning }) => {
+  return (
+    <>
+      <TextContainer bottomMargin="1.8rem">
+        <Icon
+          className={TC_DEFAULT}
+          style={{ marginInlineEnd: '0.3rem' }}
+          size="1.2rem"
+        />
+        {title}
+      </TextContainer>
+      <TextContainer color={CV_GRAY_DARK} bold bottomMargin="1rem">
+        {question}
+      </TextContainer>
+      <TextContainer color={CV_RED}>{warning}</TextContainer>
+    </>
+  );
+};
+
+export default DeleteConfirmMSG;
+
+DeleteConfirmMSG.propTypes = {
+  isOpen: PropTypes.bool,
+  onInputChange: PropTypes.func,
+  inputValue: PropTypes.string,
+  onCancelCreate: PropTypes.func,
+  onCreate: PropTypes.func,
+  modalTitle: PropTypes.string,
+  modalWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  placeholder: PropTypes.string,
+};
+DeleteConfirmMSG.defaultProps = {
+  title: '',
+  Icon: () => <></>,
+  question: '',
+  warning: '',
+};
