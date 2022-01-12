64605f6441a0c236e9c09d715696a7d94280e1b0
MaryamMaryam <m.danehvash2014@gmail.com>
2022-01-12 07:20:36 +0330

    dynamic data

--- src/views/Monitoring/MonitoringTeams/useMonitoringTeams.js
+++ src/views/Monitoring/MonitoringTeams/useMonitoringTeams.js
@@ -3,57 +3,57 @@ import {
   GetApplicationsMonitoring,
 } from 'apiHelper/apiFunctions';
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const useTeamMonitoring = (
-  appID = '36a00fac-0353-445c-be7b-df3c134e9dc7'
+const useTeamMonitoring = () =>
+  // appID = '36a00fac-0353-445c-be7b-df3c134e9dc7'
   // dateFrom = 'yyyy-mm-dd',
   // dateTo = 'yyyy-mm-dd'
-) => {
-  const [app, setApp] = useState(null);
-  const [user, setUser] = useState(null);
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  // const [hasMore, setHasMore] = useState(false);
-  const [dateFrom, setDateFrom] = useState('');
-  const [dateTo, setDateTo] = useState('');
+  {
+    const [app, setApp] = useState(null);
+    // const [ApplicationID, setApplicationID] = useState(null)
+    const [user, setUser] = useState(null);
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    // const [hasMore, setHasMore] = useState(false);
+    const [dateFrom, setDateFrom] = useState('');
+    const [dateTo, setDateTo] = useState('');
+    let { ApplicationID } = useParams();
+    console.log(ApplicationID, 'ApplicationID');
+    useEffect(() => {
+      const getData = async () => {
+        await GetApplicationPerformanceMonitoring({
+          appID: ApplicationID,
+          dateFrom: dateFrom,
+          dateTo: dateTo,
+        }).then((res) => {
+          console.log(res.Application);
+          // setHasMore(res.Applications.length > 0);
+          setData(res);
+          setUser(res.User);
+          setApp(res.Application);
+          // setApplicationID(res.Application)
+          setIsLoading(false);
+          // setHasMore(false)
+          // setHasMore(res.Applications.length > 0)
+        });
+      };
 
-  useEffect(() => {
-    const getData = async () => {
-      await GetApplicationPerformanceMonitoring({
-        appID: '36a00fac-0353-445c-be7b-df3c134e9dc7',
-        dateFrom: dateFrom,
-        dateTo: dateTo,
-      }).then((res) => {
-        console.log(res.Application);
-        // setHasMore(res.Applications.length > 0);
-        setData(res);
-        setUser(res.User);
+      getData();
+    }, [dateFrom, dateTo, ApplicationID]);
 
-        setApp(res.Application);
-
-        // if(res.TotalApplicationsCount > 0){
-        // setCounts(count=>count + 3)
-        // }
-        // console.log('hasMore', hasMore);
-        setIsLoading(false);
-        // setHasMore(false)
-        // setHasMore(res.Applications.length > 0)
-      });
+    return {
+      data,
+      app,
+      user,
+      isLoading,
+      dateFrom,
+      setDateFrom,
+      dateTo,
+      setDateTo,
+      ApplicationID,
+      // setApplicationID
     };
-
-    getData();
-  }, [appID, dateFrom, dateTo]);
-
-  return {
-    data,
-    app,
-    user,
-    isLoading,
-    dateFrom,
-    setDateFrom,
-    dateTo,
-    setDateTo,
   };
-};
 
 export { useTeamMonitoring };
