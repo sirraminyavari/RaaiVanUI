b9697ca4328ddb17a228ed508e484f54787d0d10
soren abedi <soren.abedi@gmail.com>
2022-01-28 21:31:44 +0000

    feat: :sparkles: bootstrap `StatusBadge` component
    - component has room for improvement (!)
    
    Signed-off-by: soren abedi <soren.abedi@gmail.com>

--- src/components/Badge/Badge.styles.js
+++ src/components/Badge/Badge.styles.js
@@ -1,3 +1,9 @@
+import {
+  CV_RED,
+  CV_RED_VERYSOFT,
+  TCV_DEFAULT,
+  TCV_VERY_TRANSPARENT,
+} from 'constant/CssVariables';
 import { FLEX_RCC } from 'constant/StyledCommonCss';
 import styled from 'styled-components';
 
@@ -9,4 +15,27 @@ export const BadgeWrapper = styled.div`
   text-align: center;
   font-size: ${({ length }) => (length < 3 ? '60%' : '50%')};
   direction: rtl;
+`;
+
+export const PureBadge = styled.span.attrs({
+  className: 'rv-border-radius-1',
+})`
+  padding-inline: 2rem;
+  padding-block: 0.45rem;
+  text-align: center;
+  font-size: 0.7rem;
+  font-weight: bold;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  ${({ type }) => {
+    switch (type) {
+      case 'default':
+        return `color: ${TCV_DEFAULT};
+  background-color: ${TCV_VERY_TRANSPARENT};`;
+      case 'error':
+        return `color: ${CV_RED};
+  background-color: ${CV_RED_VERYSOFT};`;
+    }
+  }}
 `;
