77521b422466002a71d7491235db79598e98c69d
m.khalafi <mehdi.khalafi@outlook.com>
2022-02-20 08:53:59 +0330

    create select obeject drop down

--- src/views/Templates/TemplateSinglePage/items/TemplateGeneralSetting/CMConfidetialitySetting/SelectObject/SelectObject.js
+++ src/views/Templates/TemplateSinglePage/items/TemplateGeneralSetting/CMConfidetialitySetting/SelectObject/SelectObject.js
@@ -1,10 +1,41 @@
-import { Button } from './SelectObjectStyle';
+import { Button, Container, DropDown, CloseButton } from './SelectObjectStyle';
+import { createRef, useState } from 'react';
+import TabView from 'components/TabView/TabView';
+import useOutsideClick from 'hooks/useOutsideClick';
+import CloseIcon from 'components/Icons/CloseIcon/CloseIcon';
 
 export const SelectObject = () => {
+  const [openDropDown, setOpenDropDown] = useState(false);
+  const ref = createRef();
+
+  useOutsideClick(() => {
+    setOpenDropDown(false);
+  }, ref);
+
   return (
-    <>
-      <Button>{'انتخاب اعضا'}</Button>
-    </>
+    <Container>
+      <Button onClick={() => setOpenDropDown(true)}>{'انتخاب اعضا'}</Button>
+
+      {openDropDown && (
+        <DropDown ref={ref}>
+          <TabView>
+            <TabView.Item label={'اعضا'}>
+              <div>{'members'}</div>
+            </TabView.Item>
+
+            <TabView.Item label={'گروه‌ها'}>
+              <div>{'groups'}</div>
+            </TabView.Item>
+
+            <TabView.Action>
+              <CloseButton>
+                <CloseIcon outline={true} size={21} />
+              </CloseButton>
+            </TabView.Action>
+          </TabView>
+        </DropDown>
+      )}
+    </Container>
   );
 };
 export default SelectObject;
--- src/views/Templates/TemplateSinglePage/items/TemplateGeneralSetting/CMConfidetialitySetting/SelectObject/SelectObjectStyle.js
+++ src/views/Templates/TemplateSinglePage/items/TemplateGeneralSetting/CMConfidetialitySetting/SelectObject/SelectObjectStyle.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { TCV_DEFAULT } from 'constant/CssVariables';
+import { CV_RED, CV_WHITE, TCV_DEFAULT } from 'constant/CssVariables';
 
 export const Button = styled.button`
   outline: none;
@@ -14,4 +14,35 @@ export const Button = styled.button`
   &:hover {
     border: 1px solid ${TCV_DEFAULT};
   }
+`;
+Button.displayName = 'Button';
+
+export const Container = styled.div`
+  max-width: 300px;
+  width: 100%;
+  position: relative;
+`;
+Container.displayName = 'Container';
+
+export const DropDown = styled.div`
+  position: absolute;
+  bottom: 2.5rem;
+  right: 0;
+  background-color: ${CV_WHITE};
+  border-radius: 0.8rem;
+  box-shadow: 1px 5px 20px #0000001f;
+  height: 300px;
+  max-width: 306px;
+  width: 100%;
+`;
+DropDown.displayName = 'DropDown';
+
+export const CloseButton = styled.button`
+  height: 1.3rem;
+  width: 1.3rem;
+  outline: none;
+  border: none;
+  background-color: transparent;
+  color: ${CV_RED};
+  cursor: pointer;
 `;
