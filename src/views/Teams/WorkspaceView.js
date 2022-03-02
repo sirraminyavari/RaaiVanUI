e76ef0a539329166ae5e4fe0280054ae6a1b2026
soren abedi <soren.abedi@gmail.com>
2022-03-02 01:42:33 +0000

    style(workspaces): :art: update workspaces view to use the `WelcomeLayout`'s **withScrollbar**
    Signed-off-by: soren abedi <soren.abedi@gmail.com>

--- src/views/Teams/WorkspaceView.js
+++ src/views/Teams/WorkspaceView.js
@@ -1,23 +1,13 @@
 import WorkspacePanels from './items/sides/content/WorkspacePanels/WorkspacePanels';
 import WelcomeBanner from './items/sides/welcome/WorkspaceWelcome';
 import WelcomeLayout from 'layouts/WelcomeLayout';
-import ScrollBarProvider from 'components/ScrollBarProvider/ScrollBarProvider';
 
 const WorkspaceView = () => {
   return (
-    <WelcomeLayout>
-      <ScrollBarProvider
-        style={{
-          position: 'relative',
-          height: '100vh',
-          overflow: 'hidden',
-          paddingInline: '1rem',
-          marginBlock: '1rem',
-        }}>
-        <div>
-          <WorkspacePanels />
-        </div>
-      </ScrollBarProvider>
+    <WelcomeLayout withScrollbar>
+      <div>
+        <WorkspacePanels />
+      </div>
       <WelcomeBanner />
     </WelcomeLayout>
   );
