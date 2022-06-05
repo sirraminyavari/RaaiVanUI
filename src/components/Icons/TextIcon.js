4731f631333a23d8074c7afb63fd54e46045406c
soren abedi <soren.abedi@gmail.com>
2022-06-05 15:51:08 +0000

    fix(node-page): :lipstick: change Icons and sizes to match the wireframe
    Signed-off-by: soren abedi <soren.abedi@gmail.com>

--- src/components/Icons/TextIcon.js
+++ src/components/Icons/TextIcon.js
@@ -4,7 +4,12 @@
 import React from 'react';
 import { GrTextAlignFull } from 'react-icons/gr';
 
-const TextIcon = ({ ...props }) => {
-  return <GrTextAlignFull {...props} />;
+const TextIcon = ({ rightSided = false, style, ...props }) => {
+  return (
+    <GrTextAlignFull
+      style={{ transform: rightSided && 'scaleX(-1)', ...style }}
+      {...props}
+    />
+  );
 };
 export default TextIcon;
