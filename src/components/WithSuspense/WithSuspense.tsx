f947711f8a5805bcaa4e6a06e4017cd46a2f4fc1
soren abedi <soren.abedi@gmail.com>
2022-06-24 12:40:19 +0000

    fix: :rotating_light: JSDoc compile error caused by `WithSuspence` and `Tooltip` components
    Signed-off-by: soren abedi <soren.abedi@gmail.com>

---
+++ src/components/WithSuspense/WithSuspense.tsx
@@ -0,0 +1,32 @@
+import type { LazyExoticComponent } from 'react';
+import { Suspense } from 'react';
+import LogoLoader from 'components/Loaders/LogoLoader/LogoLoader';
+
+/**
+ * @component - HOC for lazy loading component
+ * example:
+ *
+ * ```js
+ * const LazyComponentWithLoading = WithSuspense(lazy(() => import('views/Teams/WorkspaceView')))
+ * ```
+ * @param Component - React lazy import
+ * @param Loading - loading component e.g. LogoLoader, Shimmer or component Skeleton
+ */
+function WithSuspense<T = never>(
+  Component: LazyExoticComponent<() => JSX.Element>,
+  Loading = () => <></>
+) {
+  return (props: T) => {
+    return (
+      <Suspense
+        fallback={Loading ? <Loading /> : <LogoLoader style={undefined} />}
+      >
+        <Component {...props} />
+      </Suspense>
+    );
+  };
+}
+
+WithSuspense.displayName = 'WithSuspense';
+
+export default WithSuspense;
