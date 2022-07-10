2324425b05e79d7f198ca903247c6cce48bfcae6
DCmanager <DCmanager@nle.com>
2022-07-10 09:18:31 +0330

    implemet delete form modal

--- src/views/Templates/TemplateSinglePage/items/TemplateFormSettings/TemplateFormWrapper.js
+++ src/views/Templates/TemplateSinglePage/items/TemplateFormSettings/TemplateFormWrapper.js
@@ -8,6 +8,8 @@ import DndHandler from './items/DndHandler';
 import { useTemplateContext } from '../../TemplateProvider';
 import { useTemplateFormContext } from './TemplateFormContext';
 import { useState } from 'react';
+import Modal from 'components/Modal/Modal';
+import DeleteConfirmModal from 'components/Modal/DeleteConfirm';
 
 const TemplateFormWrapper = () => {
   const { RV_RTL: rtl } = window;
@@ -44,12 +46,36 @@ const FormDeleteButton = () => {
   const [modalInfo, setModalInfo] = useState({
     show: false,
     title: 'حذف فرم',
+    messageWarning:
+      '. پس از انتخاب حذف فرم، تمامی فیلدها حذف خواهند شد آیتم‌هایی که قبلا تکمیل شده‌اند بدون تغییر باقی خواهند .ماند ولی آیتم‌های جدید شامل فرم فعلی نخواهند بود',
+    messageTitle: 'آیا از این کار اطمینان دارید؟',
+    confirmText: 'حذف فرم',
+    cancelText: 'بازگشت',
   });
+
+  const close = () => setModalInfo({ ...modalInfo, show: false });
+
+  const open = () => setModalInfo({ ...modalInfo, show: true });
+
+  const confirm = () => {
+    // confirm
+    close();
+  };
+
   return (
-    <Button type="negative-o">
-      <TrashIcon size={17} />
-      <Styled.ButtonTitle>{'حذف فرم'}</Styled.ButtonTitle>
-    </Button>
+    <>
+      <Button type="negative-o" onClick={open}>
+        <TrashIcon size={17} />
+        <Styled.ButtonTitle>{'حذف فرم'}</Styled.ButtonTitle>
+      </Button>
+
+      <DeleteConfirmModal
+        onClose={close}
+        onCancel={close}
+        onConfirm={confirm}
+        {...modalInfo}
+      />
+    </>
   );
 };
 export default TemplateFormWrapper;
