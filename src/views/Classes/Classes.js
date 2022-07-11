1598985e31215f41b250e4aa01489eee72d1555c
sirraminyavari <sir.raminyavari@gmail.com>
2022-07-11 15:10:29 +0430

    fixed relatedId parameter issue in advanced search

--- src/views/Classes/Classes.js
+++ src/views/Classes/Classes.js
@@ -3,15 +3,12 @@ import NodeList from 'components/NodeList/NodeList';
 import { Fragment } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import useQuery from 'hooks/useQuery';
 import { useOnboardingSlice } from 'store/slice/onboarding';
 import { selectOnboarding } from 'store/slice/onboarding/selectors';
 import ProductTour from 'views/ProductTour/ProductTour';
 
 const AdvancedSearchView = ({ route }) => {
   const dispatch = useDispatch();
-  const relatedNodeID = useQuery().relatedNodeID;
-  console.log({ relatedNodeID });
 
   const { name: onboardingName } = useSelector(selectOnboarding);
   const { actions: onboardingActions } = useOnboardingSlice();
@@ -19,6 +16,7 @@ const AdvancedSearchView = ({ route }) => {
   useEffect(() => {
     if (onboardingName === 'intro')
       dispatch(onboardingActions.toggleActivation());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [route]);
 
   const nodeType =
@@ -33,11 +31,13 @@ const AdvancedSearchView = ({ route }) => {
         hierarchy={!nodeType ? [] : route?.Hierarchy || []}
         isProfile={false}
         bookmarked={route?.Bookmarked}
-        relatedNodeID={relatedNodeID}
       >
         <NodeList
           nodeTypeId={nodeType?.NodeTypeID}
           bookmarked={route?.Bookmarked}
+          relatedToNodeId={
+            route?.RelatedItem?.NodeID || route?.RelatedItem?.UserID
+          }
         />
       </AdvanceSearch>
       <ProductTour />
