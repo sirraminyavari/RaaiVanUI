41952e5062f088741a8af94be11ae2307d48801b
Soren Abedi <soren.abedi@gmail.com>
2023-04-30 11:02:28 +0330

    feat: :bug: patching bugs related to `rv-components` package
    - improve elements UI/UX
    
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/components/Buttons/ShadowButton.tsx
+++ src/components/Buttons/ShadowButton.tsx
@@ -1,9 +1,10 @@
-import { Button, RVButton, RVVariantProp } from '@cliqmind/rv-components';
+import { RVVariantProp } from '@cliqmind/rv-components';
+import Button, { IButton } from './Button';
 
 /**
  * @description a customized version of Button component that has a shadow in active mode
  */
-const ShadowButton = ({ active, ...props }: RVButton) => {
+const ShadowButton = ({ active, ...props }: IButton) => {
   return <Button active={active} variant={RVVariantProp.white} {...props} />;
 };
 
--- src/layouts/NewSidebar/MainSidebar.tsx
+++ src/layouts/NewSidebar/MainSidebar.tsx
@@ -1,5 +1,5 @@
 import { SidebarMain, SidebarSubMenu } from '@cliqmind/rv-components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import useSidebarContent from './sidebarContents/useSidebarContent';
@@ -7,6 +7,7 @@ import { selectApplication } from 'store/slice/applications/selectors';
 import { useHistory } from 'react-router-dom';
 import { decodeBase64 } from 'helpers/helpers';
 import styles from './sidebarStyles.module.scss';
+import { CLASSES_PATH, HOME_PATH } from 'constant/constants';
 
 const PrimarySidebar = () => {
   const history = useHistory();
@@ -18,19 +19,39 @@ const PrimarySidebar = () => {
     subSidebarLinks,
   } = useSidebarContent({ isSubMenuToggled, setIsSubMenuToggled, urlParams });
   const workspaceApplication = useSelector(selectApplication);
+
+  useEffect(() => {
+    console.log(CLASSES_PATH, history.location.pathname);
+
+    switch (true) {
+      case history.location.pathname.startsWith(CLASSES_PATH):
+        setIsSubMenuToggled(true);
+        break;
+      case history.location.pathname === HOME_PATH:
+        setIsSubMenuToggled(true);
+        break;
+
+      default:
+        setIsSubMenuToggled(false);
+        break;
+    }
+
+    return () => {};
+  }, [history.location, setIsSubMenuToggled]);
   return (
     <div style={{ display: 'flex' }}>
       <SidebarMain
         currentPath={history.location.pathname}
         primaryLinks={mainSidebarPrimaryLinks || []}
         secondaryLinks={mainSidebarSecondaryLinks || []}
-      ></SidebarMain>
+      />
       <SidebarSubMenu
         menuSubTitle={decodeBase64(workspaceApplication.currentApp?.Title)}
         menuTitle={decodeBase64(workspaceApplication.currentApp?.Website)}
         open={isSubMenuToggled}
         CloseTrigger={setIsSubMenuToggled}
         links={subSidebarLinks || []}
+        activeLink={history.location.pathname}
         className={styles.sidebarSubMenu}
       />
     </div>
